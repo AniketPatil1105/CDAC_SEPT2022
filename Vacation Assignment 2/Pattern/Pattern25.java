4a87c5a017d4958de0295af94d3473a561df9d83
AniketPatil1105 <113438007+AniketPatil1105@users.noreply.github.com>
2022-11-13 23:18:12 +0530

    Add files via upload

---
+++ Vacation Assignment 2/Pattern/Pattern25.java
@@ -0,0 +1,20 @@
+public class Pattern25
+{  
+public static void main(String[] args)   
+{  
+int i, j, k, rows=7;  
+for(i=1;i< rows+1 ;i++)  
+{  
+for(j=i; j < rows+1 ;j++)  
+{  
+System.out.print(j + " ");  
+}  
+for(k=1; k < i ;k++)  
+{  
+System.out.print(k + " ");  
+}  
+System.out.println();  
+}  
+}  
+} 
+
